dfbb00b7f657f7a77c46c4512bb5739d8138989e
Santhosh <santhoshs19032003@gmail.com>
2024-04-27 19:43:00 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <div className="flex w-full justify-center items-center h-screen">
+        <h1 className="text-4xl font-semibold animate-bounce">Under Development</h1>
+      </div>
+    </div>
+  );
+}
