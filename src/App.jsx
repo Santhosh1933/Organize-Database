2808f696c5864bc9b018839a9a7238febcdc18c0
Santhosh <santhoshs19032003@gmail.com>
2024-04-27 19:56:13 +0530

    Practice Branch Setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div>
       <div className="flex w-full justify-center items-center h-screen">
-        <h1 className="text-4xl font-semibold animate-bounce">Under Development</h1>
+        <h1 className="text-4xl font-semibold animate-bounce">Practice</h1>
       </div>
     </div>
   );
